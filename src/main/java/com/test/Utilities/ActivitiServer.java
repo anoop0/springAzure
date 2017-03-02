cf7f491faac44eb44257c5361be5e79feb3daf5b
Anoop Kumar <anoopk.it@gmail.com>
2017-03-02 18:33:06 +0530

    externalize

--- src/main/java/com/test/Utilities/ActivitiServer.java
+++ src/main/java/com/test/Utilities/ActivitiServer.java
@@ -4,19 +4,34 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by KumarA6 on 02-02-2017.
  */
+@Configuration
 public class ActivitiServer {
+
+    @Value("${db.type}")
+    private static String dbType;
+
+    @Value("${db.url}")
+    private static String dbURL;
+
+    @Value("${db.user}")
+    private static String dbUser;
+
+    @Value("${db.password}")
+    private static String dbPass;
 
     public static void startActiviti() {
 
         ProcessEngineConfiguration process = new StandaloneProcessEngineConfiguration();
-        process.setJdbcDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        process.setJdbcUrl("url");
-        process.setJdbcUsername("user");
-        process.setJdbcPassword("password");
+        process.setJdbcDriver(dbType);
+        process.setJdbcUrl(dbURL);
+        process.setJdbcUsername(dbUser);
+        process.setJdbcPassword(dbPass);
         process.setDatabaseSchemaUpdate("true");
         ProcessEngine engine = process.buildProcessEngine();
 
